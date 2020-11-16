33ba851623ac2599dd2f93ae9bff2e54498beb1e
Louen Pilet <louen.pilet@epitech.eu>
2020-11-16 14:53:19 +0000

    Update .eslintrc.js to integrate airbnb eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,12 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "airbnb-typescript"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "project": './tsconfig.json',
         "ecmaFeatures": {
             "jsx": true
         },
